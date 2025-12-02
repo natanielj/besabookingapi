908e1cd27a15b09d83e977e6bec81a9e680ed5d3
Nataniel Jayaseelan <njaysee@ucsc.edu>
2025-12-02 02:37:49 -0800

    origins fixed

--- api/main.py
+++ api/main.py
@@ -43,11 +43,11 @@ firebase_admin.initialize_app(firebase_cred)
 
 app = FastAPI()
 
-origins = [
-    "https://besa-booking-git-backendv5-be-student-ambassadors-projects.vercel.app", # preview environment deploy
-    "https://besa-booking.vercel.app",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+origins = ["*"
+    # "https://besa-booking-git-backendv5-be-student-ambassadors-projects.vercel.app", # preview environment deploy
+    # "https://besa-booking.vercel.app",
+    # "http://localhost:5173",
+    # "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
